75228646294c41b679a9621929a3f12b2699b144
pittalasushmitha456-coder <pittalasushmitha456@gmail.com>
2026-03-14 18:07:38 +0530

    Create FirstJavaApplication.java

---
+++ FirstJavaApplication.java
@@ -0,0 +1,7 @@
+public class FirstJavaApplication{
+  public static void main(String[] args){
+    System.out.println("Welcome to my first Java Application!");
+    System.out.println("This program demonstrate a basic java console output.);
+    System.out.println("Created by Sushmitha Pittala.");
+  }
+}
